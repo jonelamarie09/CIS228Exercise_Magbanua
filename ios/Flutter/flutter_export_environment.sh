ccfb7b3167e407255ac261e04274d6baca46b559
jonelamarie09 <126838159+jonelamarie09@users.noreply.github.com>
2023-04-28 11:54:35 +0800

    Add files via upload

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\Hp\Documents\flutter\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\Hp\Documents\CIS228\flutter activity\flutter_activity_magbanua"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Hp\Downloads\CIS228_Exercises-main\CIS228_Exercises-main"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
